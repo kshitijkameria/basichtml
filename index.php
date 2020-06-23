9553e565bf02d420695781ad551bf0f11aabf126
kshitijkameria <63162335+kshitijkameria@users.noreply.github.com>
2020-06-23 21:31:14 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,39 @@
 <html>
-<head>
-<body>
-<h1>Hello World</h1>
-</body>
-</head>
+    <head>
+        <title>
+        users
+        </title>
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <div class="col-xs-6 col-xs-offset-3">
+            <div class="panel panel-primary">
+                <div class="panel-heading">User Reg</div>
+                <div class="panel-body">
+                    <form method="post" action="user_registration_script/index.php">
+                        <div class="form-group">
+                            <label for="email_id">Email</label>
+                            <input type="email" class="form-control" id="email_id" name="email_id">
+                        </div>
+                        <div class="form-group">
+                            <label for="first_name">First Name</label>
+                            <input type="text" class="form-control" id="first_name" name="first_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="last_name">Last Name</label>
+                            <input type="text" class="form-control" id="last_name" name="last_name">
+                        </div>
+                        <div class="form-group">
+                            <label for="phone">phone</label>
+                            <input type="number" class="form-control" id="phone" name="phone">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </body>
 </html>
